c13c7ff11de6d9c3dda9cf4792dc1a72b0159c63
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-06-02 03:01:46 +0900

    firebaseの認証処理

--- app/Http/Controllers/LoginAction.php
+++ app/Http/Controllers/LoginAction.php
@@ -3,21 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Firebase\Auth\Token\Exception\InvalidToken;
+use Illuminate\Http\JsonResponse;
 
 class LoginAction extends Controller
 {
     /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * シングルアクションコントローラです。 /api/auth に POST されると、これが実行されます
+     * @param  Request  $request
+     * @return JsonResponse
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
-        //
+        $idTokenString = $request->input('idToken');
+
+        $firebase = app('firebase');
+        logger(print_r($idTokenString,true));
+        try {
+            $verifiedIdToken = $firebase->getAuth()->verifyIdToken($idTokenString);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'error!!',
+                'message' => $e->getMessage()
+            ]);
+        }
+
+        $uid = $verifiedIdToken->getClaim('sub');
+        $firebase_user = $firebase->getAuth()->getUser($uid);
         return response()->json([
-            'uid' => 'aaa',
-            'name' => 'aaa',
+            'uid' => $uid,
+            'name' => $firebase_user->displayName,
         ]);
     }
 }
